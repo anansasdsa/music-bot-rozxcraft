58a64959d29c73ee620975edb51206052a24adea
anansasdsa <41487794+anansasdsa@users.noreply.github.com>
2018-08-22 03:40:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'التوكن حق بوتك ';
+exports.TOKEN = 'NDcyMzgyNzUwMTYzNDY4MzA4.Dl5B-w.w9i-oHqixtT66hQ5vAV3oXfs3hs';
 
-exports.PREFIX = 'البريفكس';
+exports.PREFIX = '*';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';//لا تجيه
