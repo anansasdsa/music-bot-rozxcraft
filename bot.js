99659beff04fe5c1556c55f3a9a53dfd9b106864
anansasdsa <41487794+anansasdsa@users.noreply.github.com>
2019-01-22 01:04:23 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const bot = require('discord.js-musicbot-addon');//Elmusaui_GK + Narox
 bot.start(client, {//Elmusaui_GK + Narox
  youtubeKey: "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8", //Elmusaui_GK + Narox
- musichelp : "*help", //Elmusaui_GK + Narox
+ musichelp : "*help-music", //Elmusaui_GK + Narox
  botPrefix : "*", //Elmusaui_GK + Narox
  anyoneCanSkip: true, // اي احد يقدر يسوي سكيب : true // ما حد يقدر يسوي سكيب الا الي عنده امر السكيب : false 
  bigPicture : true, //Elmusaui_GK + Narox
