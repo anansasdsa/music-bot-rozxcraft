91cdc064c0589c54849220df7bdd7334835d7bf4
anansasdsa <41487794+anansasdsa@users.noreply.github.com>
2019-01-22 01:02:34 -0800

    Update and rename config.js to bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const bot = require('discord.js-musicbot-addon');//Elmusaui_GK + Narox
+bot.start(client, {//Elmusaui_GK + Narox
+ youtubeKey: "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8", //Elmusaui_GK + Narox
+ musichelp : "*help", //Elmusaui_GK + Narox
+ botPrefix : "*", //Elmusaui_GK + Narox
+ anyoneCanSkip: true, // اي احد يقدر يسوي سكيب : true // ما حد يقدر يسوي سكيب الا الي عنده امر السكيب : false 
+ bigPicture : true, //Elmusaui_GK + Narox
+ anyoneCanAdjust : true, //حد يقدر يغير الصوت اذا خليته true // ما حد يقدر يغير الصوت الا الي عندهم ادمينيستراتور : false 
+});//Elmusaui_GK + Narox
+
+
+
+
+client.login('NTM2OTY1MzA4OTU5ODE3NzM4.DyeemA.LHbU3Y9T4W5-ob-5pt-gD_eFftE');
--- config.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'NTIxNDA0MDg4Njg0OTA0NDY4.DvAEMg.4XVkf5iB2A9wRQBHCPxEU8qpqc4';
-
-exports.PREFIX = '$';
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';//لا تجيه
